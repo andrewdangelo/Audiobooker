a2d95539307966d7dd4530c01bd2818d636e282b
Muhammad Raqib <muhammadraqib@Muhammads-MacBook-Air.local>
2025-11-30 13:51:07 -0500

    adding in default for ID field in users

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -1,19 +1,13 @@
-"""
-User Database Model
-TODO: Implement user authentication and authorization
-"""
 
 from sqlalchemy import Column, String, Boolean, DateTime
 from sqlalchemy.sql import func
 from config.database import Base
-
+import uuid
 
 class User(Base):
-    """User model"""
-    
     __tablename__ = "users"
-    
-    id = Column(String, primary_key=True, index=True)
+
+    id = Column(String, primary_key=True, index=True, default=lambda: str(uuid.uuid4()))
     email = Column(String, unique=True, index=True, nullable=False)
     username = Column(String, unique=True, index=True, nullable=False)
     hashed_password = Column(String, nullable=False)
